edf35d7d684ce818a9d2ec857290384269fbcd28
HoFerreira <heigthx@gmail.com>
2025-01-18 11:17:12 -0300

    Enviando o código

--- src/CalculadoraIMC.java
+++ src/CalculadoraIMC.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class CalculadoraIMC {
     public static void main(String[] args) {
-
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Olá, por favor digite o seu nome");
